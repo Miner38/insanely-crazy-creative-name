7e915dcc39e5c3bd36a8b4a46e57f390a79e442e
Miner38 <89616461+Miner38@users.noreply.github.com>
2023-12-15 06:25:10 +0000

    Add files via upload

---
+++ script.sh
@@ -0,0 +1,11 @@
+# /bin/bash
+
+cd ~/.mozilla/firefox/
+cd *.default-release
+touch users.js
+printf "user_pref(\"browser.sessionstore.resume_from_crash\", false);\n" > users.js
+echo "HTML uri of project please"
+read uri
+printf "user_pref(\"browser.startup.homepage\", \"" > users.js
+printf uri > users.js
+printf "\");\n" > users.js
